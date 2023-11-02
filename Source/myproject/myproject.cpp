c4f8668b6593acb7eb0843160718e374c0d0bbd6
Abelabc <2542503181@qq.com>
2023-11-02 21:53:46 +0800

    up-homework2

--- Source/myproject/myproject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "myproject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, myproject, "myproject" );
